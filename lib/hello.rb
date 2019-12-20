fc2cc4beaf937883e68742f8d61c9f62b98fe192
Will Carter <will.carter@fergusllc.com>
2019-12-20 19:55:46 +0000

    Automatically backed up by Learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -6,25 +6,24 @@ def hello_t(array)
   end
 end
 
-
-
-# hello_t(myArray) { |name|
-#   if name.start_with?("T")
-#     puts "Hi, #{name}"
-#   end
-# }
-
-def hello_2(array)
-  i = 0
-  while i < array.length
-    if array[i].start_with?("T")
-      puts "Hi, #{array[i]}"
-    end
-    i = i + 1
-  end
-end
-
 myArray = ["Tim", "Tom", "Jim"]
-hello_2(myArray)
+hello_t(myArray) { |name|
+  if name.start_with?("T")
+    puts "Hi, #{name}"
+  end
+}
+
+# def hello_2(array)
+#   i = 0
+#   while i < array.length
+#     if array[i].start_with?("T")
+#       puts "Hi, #{array[i]}"
+#     end
+#     i = i + 1
+#   end
+# end
+#
+# myArray = ["Tim", "Tom", "Jim"]
+# hello_2(myArray)
 
 # call your method here!
